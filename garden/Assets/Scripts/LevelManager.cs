639862e36097f44325061cea0bd2303e614ea79a
triglaw <hazelnutninja@gmail.com>
2017-06-06 18:36:36 +0200

    first commit

---
+++ garden/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public float autoloadNextLevelAfter = 0f;
+
+    void LoadLevel(string name)
+    {
+        Debug.Log("New Level load: " + name);
+        SceneManager.LoadScene(name);
+    }
+
+    public void QuitRequest() {
+        Application.Quit();
+    }
+
+    public void LoadNextLevel() {
+        SceneManager.LoadScene(sceneBuildIndex);
+    }
+}
